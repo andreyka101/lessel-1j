6703d38dd4fc6762609a976f6e95d856af6a6576
andreyka101 <andrejkvasnin236@gmail.com>
2023-03-30 19:34:53 +0300

    grg

--- script.ts
+++ script.ts
@@ -1,8 +1,22 @@
-/*
-let win=prompt('ert')
-console.log(win)
-if (win!=null){
 
-  win=win**2
-}
-*/
+// let win=parseInt(prompt('я могу посчитать степень 2х') as string)
+// if (win || win == 0){
+//    win=win**2
+//    alert('ответ '+win)
+//   }
+//   else
+//   alert('ну ок')
+  
+
+
+  // win=parseInt(prompt('число 1') as string)
+  // let sum=win
+  // win=parseInt(prompt('число 2') as string)
+  // sum=(sum+win)/2
+  // alert(sum)
+  let w=5
+  while(w>1){
+    console.log(w)
+    w++
+
+  }
