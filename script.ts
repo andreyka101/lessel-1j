0f403e550937c313e58a8ff2df26c24954f97b0b
andreyka101 <andrejkvasnin236@gmail.com>
2023-04-17 08:28:51 +0300

    sfeffe

--- script.ts
+++ script.ts
@@ -15,17 +15,23 @@ function n1 (){
 
 //  2
 
-// let win=parseInt(prompt('число 1') as string)
-// let sum=win
-// win=parseInt(prompt('число 2') as string)
-// sum=(sum+win)/2
-// alert(sum)
+function n2 (){
+  let win=parseInt(prompt('число 1') as string)
+  let sum=win
+  win=parseInt(prompt('число 2') as string)
+  sum=(sum+win)/2
+  alert(sum)
+}
+// n2()
 
 //  5
 
-// let num= +(prompt('число 1') as string)
-// let num22= +(prompt('число 2') as string)
-// alert('ответ сумм ' + (num + num22) + '\n ответ минус ' + (num - num22) + '\n ответ деление ' + (num / num22) + '\n ответ умножение ' + (num * num22) + '\n введеные числа ${num} ${num22}')
+function n3 (){
+  let num= +(prompt('число 1') as string)
+  let num22= +(prompt('число 2') as string)
+  alert('ответ сумм ' + (num + num22) + '\n ответ минус ' + (num - num22) + '\n ответ деление ' + (num / num22) + '\n ответ умножение ' + (num * num22) + '\n введеные числа ${num} ${num22}')
+}
+// n3()
 
 
 
